ba4c314bec22483cede532aaff6940736254f0cb
Diarrah <diarrahcasiano@gmail.com>
2021-06-21 15:11:44 -0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home, Headphones, Speakers, Earphones, Cart } from './pages';
+import { Navbar, Footer } from './components';
+
 const App = () => {
   return (
-    <div className="App">
-        Hi
-    </div>
+    <Router>
+        <div className="App">
+            <Navbar />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Headphones exact path="/headphones" component={Headphones} />
+                <Speakers exact path="/speakers" component={Speakers} />
+                <Earphones exact path="/earphones" component={Earphones} />
+                <Cart exact path="/cart" component={Cart} />
+            </Switch>
+            <Footer />
+        </div>
+    </Router>
   );
 }
 
